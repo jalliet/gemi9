6496f05560a00c1a8adade53c59cdefd169abe6d
EmmanuelAdio <emmanueladio065@gmail.com>
2024-10-12 15:57:52 +0100

    route updating

--- gemi_nine/src/App.js
+++ gemi_nine/src/App.js
@@ -7,6 +7,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import QuizPage from './components/QuizPage';
+import PhotoPage from './components/photoPage';
 
 const App = () => {
   return (
@@ -18,6 +19,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           {/* Quiz page route */}
           <Route path="/quiz" element={<QuizPage />} />
+          {/* Photo page route */}
+          <Route path="/photo" element={<PhotoPage />} />
         </Routes>
       </div>
     </Router>
