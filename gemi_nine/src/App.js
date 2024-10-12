8bb9873a329dff2c4560d5608e87c8fe0b85ea7a
EmmanuelAdio <emmanueladio065@gmail.com>
2024-10-12 15:18:30 +0100

    added bootstrap

--- gemi_nine/src/App.js
+++ gemi_nine/src/App.js
@@ -1,5 +1,8 @@
 // import logo from './logo.svg';
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 import QuizCard from './QuizCard';
 import React, { useState, useEffect } from 'react';
