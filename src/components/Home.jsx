bacd2ebbf1d190f7518385088b30d8bb453a1150
dzakd9 <denised.9@hotmail.com>
2024-10-13 16:27:25 +0100

    user page and card addition

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import GEMI9Logo from "./GEMI9_logo.png";
+import userPhoto from "./user-photo.png"
 
 const Home = () => {
   return (
@@ -8,14 +9,34 @@ const Home = () => {
       
       <header>
         <nav>
-          <img className = "gemi9_logo" src = {GEMI9Logo} alt = "gemi9"/> 
-          <div> 
-          <h1 className = "logo-name"> SPOTLIGHT</h1>
-          <p> Shining a Light on Black Talent</p> 
+          <div className = "nav-div">
+            <div className = "pic-title-div">
+              <img className = "gemi9_logo" src = {GEMI9Logo} alt = "gemi9"/> 
+              <div className = "title-div"> 
+                <h1 className = "logo-name"> SPOTLIGHT</h1>
+                <p> Shining a Light on Black Excellence </p> 
+              </div>
+            </div>
+
+            <div className = "user-div">
+              <img className = "user-photo" src = {userPhoto} alt = "user image"/>
+            </div>
+    
           </div>
   
         </nav>
       </header>
+
+        <div class="card">
+          <div className ="container">
+            <h4><b>Firstname Surname</b></h4>
+          </div>
+          <div className = "container">
+            <p> High Score: 10 </p>
+            <p> Streak:  7 days </p>
+          </div>  
+        </div>
+    
 
     <section className="home-buttons flex-column">
       {/* Link to the quiz page */}
