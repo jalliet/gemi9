40e2bbb6d6966bc44c8659554ab396bb3dd13e5b
dzakd9 <denised.9@hotmail.com>
2024-10-13 14:44:39 +0100

    getting the logo again

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GEMI9Logo from "./GEMI9_logo.png";
 
 const Home = () => {
   return (
@@ -7,8 +8,12 @@ const Home = () => {
       
       <header>
         <nav>
-          <h1> Spotlight</h1>
-          <p> Shining a Light on Black Talent</p>
+          <img className = "gemi9_logo" src = {GEMI9Logo} alt = "gemi9"/> 
+          <div> 
+          <h1 className = "logo-button"> SPOTLIGHT</h1>
+          <p> Shining a Light on Black Talent</p> 
+          </div>
+  
         </nav>
       </header>
 
@@ -17,7 +22,15 @@ const Home = () => {
       <section> <center> <Link to="/quiz" className=" btn primary-button"> Start Quiz</Link> </center> </section>
       <section> <center> <Link to="/photo" className="btn primary-button">  Upload a Photo </Link> </center> </section>
     </section>
-    
+
+   
+     <div class="footer"> 
+     <p>
+      Created by GEMI9
+     </p>
+     </div>
+
+
     </body>
   );
 };
